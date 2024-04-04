bb82c9e4d778c152f880110e685c7eaa34bdc591
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-04-04 16:40:20 -0400

    add hello

---
+++ examples/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(int argc, char **argv) {
+    printf("Hello World from Coulson's hello.c\n");
+    return EXIT_SUCCESS;
+}
